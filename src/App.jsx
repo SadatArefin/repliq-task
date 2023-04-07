982c0062f369dcf3b266957f6f3f0b399b43112c
SadatArefin <sadat.arefin.rafat@gmail.com>
2023-04-07 14:29:37 +0600

    Admin Dashboard page and Routing update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>Hello</div>
+      <Header/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Registration from "./components/Registration/Registration";
+import AdminDashboard from "./components/AdminDashboard/AdminDashboard";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -14,6 +17,18 @@ const router = createBrowserRouter([
       </div>
     ),
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/registration",
+    element: <Registration />,
+  },
+  {
+    path: "/admin",
+    element: <AdminDashboard />,
+  }
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
