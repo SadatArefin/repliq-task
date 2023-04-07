4ae61bd3949f62cf965fd0c7226d16197ae5f3c0
SadatArefin <sadat.arefin.rafat@gmail.com>
2023-04-07 14:28:44 +0600

    Registration with Basic Styling

---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import Header from "../Header/Header";
+
+const Registration = () => {
+  return (
+    // Registration form with tailwind with normalizePath, email, password, password confirmation, and submit button with google and facebook buttons
+    <div>
+      <Header />
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-white rounded-lg shadow-xl p-10">
+          <h2 className="text-3xl font-bold mb-10 text-center">Registration</h2>
+          <form>
+            <div className="mb-5">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="mb-5">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Confirm Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Confirm Password"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Register
+              </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="/login"
+              >
+                Login
+              </a>
+            </div>
+          </form>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Register with Google
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Register with Facebook
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Registration;
